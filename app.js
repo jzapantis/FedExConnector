d5342407b678e0805a750834f011b9213b362b37
jzapantis <jzapantis@gmail.com>
2016-11-20 01:40:29 -0500

    got the back end working properly for bulk insert with tracking numbers with MongoDB.

--- app.js
+++ app.js
@@ -27,87 +27,54 @@ app.listen(appEnv.port, '0.0.0.0', function () {
   console.log("server starting on " + appEnv.url);
 });
 ////////////////////////////////////////
-var db = require("./lib/db/crud/index.js");
+// var db = require("./lib/db/crud/index.js");
+////////////////////////////////////////
+var Db = require('mongodb').Db,
+  MongoClient = require('mongodb').MongoClient,
+  Server = require('mongodb').Server,
+  ReplSetServers = require('mongodb').ReplSetServers,
+  ObjectID = require('mongodb').ObjectID,
+  Binary = require('mongodb').Binary,
+  GridStore = require('mongodb').GridStore,
+  Grid = require('mongodb').Grid,
+  Code = require('mongodb').Code,
+  // BSON = require('mongodb').pure().BSON, ---- NOT WORKING FOR SOME REASON
+  assert = require('assert');
+
+
 ////////////////////////////////////////
 app.post('/testGoogle', function (req, res) {
   var doc = req.body;
   var newTrackingNumbers = doc.trackingNumbers;
-  var rawTrackingNumbers = [];
-
-  if (req.body.type === "array") {
-
-    var insertDoc = {
-      timeStamp: "",
-      trackingNumber: "",
-      email: ""
-    };
-
+  console.log("type of incoming tracking numbers variable: ", typeof newTrackingNumbers)
+  console.log("its says object, so lets see the keys: ", Object.keys(newTrackingNumbers))
+  console.log("the first tracking number in the list: ", newTrackingNumbers[0])
+  var insertDocs = [];
+  console.log("in endpoint, req.body = ", req.body)
+  console.log("back-end received an array of tracking numbers: ", req.body.trackingNumbers)
+  var insertDoc = {
+    timeStamp: "",
+    trackingNumber: "",
+    email: ""
+  };
+  console.log("quantity of new inserts: ", newTrackingNumbers.length)
+  var db = new Db('test', new Server('localhost', 27017));
+  db.open(function (err, db) {
+    var col = db.collection('ITOrders');
+    var batch = col.initializeUnorderedBulkOp({ useLegacyOps: true });
     for (var i = 0; i < newTrackingNumbers.length; i++) {
-      insertDoc.timestamp = doc.timeStamp;
+      console.log("NEW INSERT ####################")
+      insertDoc.timeStamp = doc.timeStamp;
       insertDoc.email = doc.submissionEmail;
       insertDoc.trackingNumber = newTrackingNumbers[i];
-
-      db.insert(insertDoc, function (err, res) {
-        if (err) {
-          console.log('[fedEx.insert error] ', err);
-          res.send("error: ", err);
-          return
-        }
-        if (!err) {
-          rawTrackingNumbers.push = newTrackingNumbers[i];
-          console.log(rawTrackingNumbers[i]);
-        }
-      });
+      console.log("document being inserted into the DB: ", insertDoc)
+      console.log("current tracking number being inserted: ", newTrackingNumbers[i])
+      insertDocs.push(insertDoc);
+      batch.insert(insertDoc);
     }
-    console.log(rawTrackingNumbers);
-    res.send(rawTrackingNumbers);
-
-  } else {
-    db.insert(insertDoc, function (err, res) {
-      if (err) {
-        console.log('[mongoDB insert error] ', err);
-        res.send("error: ", err);
-      }
-      if (!err) {
-        console.log(body.trackingNumbers);
-        res.send(body.trackingNumbers);
-      }
-    });
-  }
-});
-
-app.get('/getTrackingNumbers', function (req, res) {
-
-  var usr = '148f1d3e-9ac8-4b76-be8b-5f89ee464ffc-bluemix';
-  var pwd = 'e96ccc3165556303402511e315c0b52cd9bbcbc5d48042697ad122e0646db1aa';
-
-  var cloudant = Cloudant({
-    account: usr,
-    password: pwd
-  });
-
-  var fedEx = cloudant.db.use('tracking-numbers');
-
-  var query = {
-    "selector": {
-      "_id": {
-        "$gt": 0
-      }
-    },
-    "sort": [{
-      "_id": "asc"
-    }]
-  }
-
-  fedEx.find(query, function (error, result) {
-    if (error) {
-      console.log("Request did not return a 200, or there was error: ", error);
-      res.send("error: ", error)
-    }
-    if (!error) {
-      console.log(result);
+    batch.execute(function (err, result) {
       res.send(result);
-    }
-    return;
+      db.close();
+    });
   });
 });
